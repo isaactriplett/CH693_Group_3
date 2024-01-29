3966a58fa4d40f528b599ef00054a254a90cd050
Isaac Triplett <isaactriplett21@live.com>
2024-01-28 22:29:31 -0800

    added some quality of life features to CVGrapher

--- CVGrapher.py
+++ CVGrapher.py
@@ -14,12 +14,17 @@ from matplotlib.ticker import (MultipleLocator, AutoMinorLocator)     # Configur
 #same general layout as Nyquist grapher. Make sure to replace my file path with your own.
 
 def CV_grapher(const #Normalizing constant to turn current input into current density output if desired
-               ,filenames,title,xlimits,yunits,ylimits,txt): #txt is true if dealing with a txt file, and false if dealing with a csv
-    
+               ,filenames,title,xlimits,yunits,ylimits,txt,currentDependence,xcolumn,ycolumn): #txt is true if dealing with a txt file, and false if dealing with a csv
+#currentDependence: True if current is on the y axis.    
+
     fig = plt.figure(figsize=(8, 6))    # Create a graph 'fig' which has 4 inches in width and 6 inches in height.
     ax = fig.add_subplot(111)           # Create a subplot 'ax' in the figure 'fig'. 
-    ax.set_ylabel('Potential (V)')   # set the label of the x-axis
-    ax.set_xlabel('Current ' + yunits) # set the label of the y-axis
+    if currentDependence == True:
+        ax.set_ylabel('Current ' + yunits)
+        ax.set_xlabel('Potential (V)')
+    else:
+        ax.set_ylabel('Potential (V)')   # set the label of the x-axis
+        ax.set_xlabel('Current ' + yunits) # set the label of the y-axis
     
     X1 = []
     X2 = []
@@ -41,12 +46,19 @@ def CV_grapher(const #Normalizing constant to turn current input into current de
     i=0
     while(i < len(filenames)):
         if txt == False:
-            datalistY[i],datalistX[i]=np.loadtxt(fname="C:/Users/isaac/OneDrive/Documents/Electrochemistry Program/Lab 2/Project 1/Week 1/Data/CH693 Project 1 Week 1/CH693 Project 1 Week 1/" + filenames[i] + ".csv", delimiter = ',', skiprows=1, unpack=True,
-                   usecols=(1,2))
-                     # Read data from a file scan1.csv and skip the first row.
+            if currentDependence == True:
+                datalistX[i],datalistY[i]=np.loadtxt(fname="C:/Users/isaac/OneDrive/Documents/Electrochemistry Program/Lab 2/Project 1/Week 2/" + filenames[i] + ".csv", skiprows=1, unpack=True,delimiter=',',
+                                                     usecols=(xcolumn,ycolumn))
+            else:
+                datalistY[i],datalistX[i]=np.loadtxt(fname="C:/Users/isaac/OneDrive/Documents/Electrochemistry Program/Echem Project/Data/txt files/Week 3/" + filenames[i] + ".csv", skiprows=1, unpack=True,
+                                                     delimiter=',',usecols=(0,1))
         elif txt == True:
-            datalistX[i],datalistY[i]=np.loadtxt(fname="C:/Users/isaac/OneDrive/Documents/Electrochemistry Program/Lab/Project 5/Data Files/" + filenames[i] + ".txt", skiprows=1, unpack=True,
-                   usecols=(0,1))
+            if currentDependence == True:
+                datalistX[i],datalistY[i]=np.loadtxt(fname="C:/Users/isaac/OneDrive/Documents/Electrochemistry Program/Echem Project/Data/txt files/Week 3/" + filenames[i] + ".txt", skiprows=1, unpack=True,
+                                                     usecols=(0,1))
+            else:
+                datalistY[i],datalistX[i]=np.loadtxt(fname="C:/Users/isaac/OneDrive/Documents/Electrochemistry Program/Echem Project/Data/txt files/Week 3/" + filenames[i] + ".txt", skiprows=1, unpack=True,
+                                                     usecols=(0,1))
         ax.plot(datalistX[i], datalistY[i]/const, '.', color=colorlist[i],label=filenames[i])
         i = i + 1
         
@@ -65,7 +77,6 @@ def CV_grapher(const #Normalizing constant to turn current input into current de
     ax.xaxis.set_minor_locator(MultipleLocator(10))   # add minor ticks for the x-axis
     ax.yaxis.set_minor_locator(AutoMinorLocator())    # add minor ticks for the y-axis
     ax.xaxis.grid(True, which='both') # add grids to the x-axis for both major and minor ticks
-    
     if title != None:
         ax.set_title(title)
 
